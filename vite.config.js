2d6ecf5bfbabd5bd3c5555a87a8837fdb043d4a4
syafiqahnasir <snasir@nextlabs.com>
2026-09-05 17:38:54 +0800

    Configure Vite with GitHub Pages SPA plugin and base path

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { githubPagesSpa } from '@sctg/vite-plugin-github-pages-spa';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()], // tailwind css plugin
+  plugins: [react(), tailwindcss(), githubPagesSpa()], // plugin
   base: '/modern-todolist-app/', // repository name
 })
